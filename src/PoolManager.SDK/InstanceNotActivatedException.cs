507b2bc0384f7209bac1f9c811d84ad24c280092
Charles Zipp <charles.zipp@wolterskluwer.com>
2018-08-30 16:02:24 -0500

    added PooledServiceProxy for integration into gateway and engagements

---
+++ src/PoolManager.SDK/InstanceNotActivatedException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PoolManager.SDK
+{
+    [Serializable]
+    public class InstanceNotActivatedException : Exception
+    {
+        public InstanceNotActivatedException(string serviceTypeUri, string serviceInstanceName)
+            : base("The instance has not completed activation, please try again later")
+        {
+            ServiceTypeUri = serviceTypeUri;
+            ServiceInstanceName = serviceInstanceName;
+        }
+
+        protected InstanceNotActivatedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            ServiceTypeUri = info.GetString(nameof(ServiceTypeUri));
+            ServiceInstanceName = info.GetString(nameof(ServiceInstanceName));
+        }
+
+        public string ServiceTypeUri { get; }
+
+        public string ServiceInstanceName { get; }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(ServiceTypeUri), ServiceTypeUri);
+            info.AddValue(nameof(ServiceInstanceName), ServiceInstanceName);
+            base.GetObjectData(info, context);
+        }
+    }
+}
