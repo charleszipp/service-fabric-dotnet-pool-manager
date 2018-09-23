348aaf9a349f3a3f6c67270d6980d1183bb1c9d0
Charles Zipp <charles.zipp@wolterskluwer.com>
2018-09-23 13:50:14 -0500

    added service to project.. still need to adjust logic to detect orphans

---
+++ src/PoolManager.Monitor/Models/OrphanInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Fabric.Health;
+using System.Fabric.Query;
+using System.Runtime.Serialization;
+
+namespace PoolManager.Monitor.Models
+{
+    /// <summary>
+    /// This structure holds service information for an orphaned service instance
+    /// </summary>
+    [DataContract]
+    public class OrphanInfo
+    {
+        /// <summary>
+        /// Name of the service instance
+        /// </summary>
+        [DataMember]
+        public string ServiceName { get; private set; }
+
+        /// <summary>
+        /// The service type uri this orphan is for
+        /// </summary>
+        public string ServiceTypeUri { get; private set; }
+
+        /// <summary>
+        /// Service current health state
+        /// </summary>
+        [DataMember]
+        public HealthState HealthState { get; private set; }
+
+        /// <summary>
+        /// Service current status
+        /// </summary>
+        [DataMember]
+        public ServiceStatus ServiceStatus { get; private set; }
+
+        public OrphanInfo(string serviceName, string serviceTypeUri, HealthState healthState, ServiceStatus serviceStatus)
+        {
+            ServiceName = serviceName;
+            ServiceTypeUri = serviceTypeUri;
+            HealthState = healthState;
+            ServiceStatus = serviceStatus;
+        }
+    }
+}
---
+++ src/PoolManager.Monitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Ninject;
+using PoolManager.Monitor.Modules;
+
+namespace PoolManager.Monitor
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ServiceRuntime.RegisterServiceAsync("MonitorType",
+                    context => new StandardKernel(new ServiceModule(context)).Get<PoolManagerMonitorService>())
+                .GetAwaiter()
+                .GetResult();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
