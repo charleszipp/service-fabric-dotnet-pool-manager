d0c1b91346462351b02a778e56f785cdbfb0b1eb
Charles Zipp <charles.zipp@wolterskluwer.com>
2018-09-24 06:37:52 -0500

    setup ops endpoint initial project

---
+++ src/PoolManager.Web/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ServiceFabric.Services.Runtime;
+using System.Threading;
+
+namespace PoolManager.Web
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ServiceRuntime.RegisterServiceAsync("WebType",
+                    context => new Web(context)).GetAwaiter().GetResult();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ src/PoolManager.Web/Web.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.ServiceFabric;
+using Microsoft.ApplicationInsights.ServiceFabric.Module;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace PoolManager.Web
+{
+    internal sealed class Web : StatelessService
+    {
+        public Web(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "internal", (url, listener) =>
+                    {
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton(serviceContext)
+                                            .AddSingleton<ITelemetryInitializer>((serviceProvider) => new FabricTelemetryInitializer())
+                                            .AddSingleton<ITelemetryInitializer>((serviceProvider) => FabricTelemetryInitializerExtension.CreateFabricTelemetryInitializer(serviceContext))
+                                            .AddSingleton<ITelemetryModule>(new ServiceRemotingDependencyTrackingTelemetryModule())
+                                            .AddSingleton<ITelemetryModule>(new ServiceRemotingRequestTrackingTelemetryModule())
+                                        )
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseApplicationInsights()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.UseReverseProxyIntegration)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+    }
+}
