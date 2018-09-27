8bb97deaf547a0dc4834807d2ac52662e1549404
Charles Zipp <charles.zipp@wolterskluwer.com>
2018-09-27 10:32:04 -0500

    added pools controller

---
+++ src/PoolManager.Web/Api/Pools/Pool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace PoolManager.Web.Api.Pools
+{
+    [DataContract]
+    public class Pool
+    {
+        public Pool(PoolConfiguration configuration, IEnumerable<Guid> vacantInstances)
+        {
+            Configuration = configuration;
+            VacantInstances = vacantInstances;
+        }
+
+        [DataMember]
+        public PoolConfiguration Configuration { get; private set; }
+
+        [DataMember]
+        public IEnumerable<Guid> VacantInstances { get; private set; }
+    }
+}
---
+++ src/PoolManager.Web/Api/Pools/PoolsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PoolManager.SDK.Pools;
+using PoolManager.SDK.Pools.Responses;
+
+namespace PoolManager.Web.Api.Pools
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoolsController : ControllerBase
+    {
+        private readonly IPoolProxy pools;
+
+        public PoolsController(IPoolProxy pools)
+        {
+            this.pools = pools;
+        }
+
+        [HttpGet("{serviceTypeUri}", Name = "GetPool")]
+        public async Task<ActionResult<Pool>> Get(string serviceTypeUri)
+        {
+            ConfigurationResponse cfg = null;
+            try
+            {
+                cfg = await pools.GetConfigurationAsync(serviceTypeUri);
+            }
+            catch(InvalidOperationException ex) when (ex.Message.ToLower().Contains("no configuration"))
+            {
+                return NotFound($"Pool {serviceTypeUri} does not exist or has not yet been started.");
+            }
+            
+            var getVacantInstancesResult = await pools.GetVacantInstancesAsync(serviceTypeUri);
+            
+            var response = new Pool(
+                new PoolConfiguration(cfg.ExpirationQuanta, cfg.HasPersistedState, cfg.IdleServicesPoolSize,
+                    cfg.IsServiceStateful, cfg.MaxPoolSize, cfg.MinReplicaSetSize,
+                    (PartitionSchemeDescription)Enum.Parse(typeof(PartitionSchemeDescription), cfg.PartitionScheme.ToString()),
+                    cfg.ServicesAllocationBlockSize, cfg.TargetReplicasetSize),
+                getVacantInstancesResult.VacantInstances
+            );
+
+            return Ok(response);
+        }
+    }
+}
